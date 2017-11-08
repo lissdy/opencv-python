e877a08d3a0ba317e8ff8c2273e1acbc49615652
lisa <allen.h.chen@pwc.com>
2017-11-08 13:27:35 +0800

    bgr to rgb

--- affine_transformation.py
+++ affine_transformation.py
@@ -4,13 +4,20 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img=cv2.imread('demo.jpg',0)
-rows,cols = img.shape
+
+img_bgr = cv2.imread('demo.jpg')
+img_rgb = np.zeros(img_bgr.shape, img_bgr.dtype)
+img_rgb[:,:,0] = img_bgr[:,:,2]
+img_rgb[:,:,1] = img_bgr[:,:,1]
+img_rgb[:,:,2] = img_bgr[:,:,0]
+
+
+rows,cols,ch = img_rgb.shape
 pts1 = np.float32([[50,50],[200,50],[50,200]])
 pts2 = np.float32([[10,100],[200,50],[100,250]])
 
 M = cv2.getAffineTransform(pts1,pts2)
-dst = cv2.warpAffine(img,M,(cols,rows))
-plt.subplot(121),plt.imshow(img),plt.title('Input')
+dst = cv2.warpAffine(img_rgb,M,(cols,rows))
+plt.subplot(121),plt.imshow(img_rgb),plt.title('Input')
 plt.subplot(122),plt.imshow(dst),plt.title('Output')
 plt.show()
