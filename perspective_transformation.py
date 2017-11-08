6e9c102a346ca85e9c0bf955c55fda9a36ad48fc
lisa <allen.h.chen@pwc.com>
2017-11-08 10:31:22 +0800

    adaptive thresholding

--- perspective_transformation.py
+++ perspective_transformation.py
@@ -6,7 +6,7 @@ from matplotlib import pyplot as plt
 
 img = cv2.imread('grid.png')
 rows,cols,ch = img.shape
-pts1 = np.float32([[56,65],[368,52],[28,387],[389,390]])
+pts1 = np.float32([[71,82],[492,67],[31,512],[519,515]])
 pts2 = np.float32([[0,0],[300,0],[0,300],[300,300]])
 M = cv2.getPerspectiveTransform(pts1,pts2)
 dst = cv2.warpPerspective(img,M,(300,300))
